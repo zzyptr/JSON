414127f1b2d85d4231c4e6ce6dfb87c787e0b43e
机智勇敢 <jon@jon15.local>
2021-06-15 15:43:56 +0800

    Removing JSONConvertible

--- Sources/JSON/JSON+Expressible.swift
+++ Sources/JSON/JSON+Expressible.swift
@@ -33,65 +33,16 @@ extension JSON: ExpressibleByStringLiteral {
 extension JSON: ExpressibleByArrayLiteral {
 
     @inlinable
-    public init(arrayLiteral elements: JSONConvertible...) {
-        self = .array(elements.map(\.json))
+    public init(arrayLiteral elements: JSON...) {
+        self = .array(elements)
     }
 }
 
 extension JSON: ExpressibleByDictionaryLiteral {
 
     @inlinable
-    public init(dictionaryLiteral elements: (String, JSONConvertible)...) {
-        let dictionary = elements.reduce(into: [:]) { result, element in
-            result[element.0] = element.1.json
-        }
+    public init(dictionaryLiteral elements: (String, JSON)...) {
+        let dictionary = Dictionary(elements, uniquingKeysWith: { $1 })
         self = .dictionary(dictionary)
     }
-}
-
-public protocol JSONConvertible {
-
-    var json: JSON { get }
-}
-
-extension JSON: JSONConvertible {
-
-    @inlinable
-    public var json: JSON { self }
-}
-
-extension Bool: JSONConvertible {
-
-    @inlinable
-    public var json: JSON { .bool(self) }
-}
-
-extension Int: JSONConvertible {
-
-    @inlinable
-    public var json: JSON { .int(self) }
-}
-
-extension Double: JSONConvertible {
-
-    @inlinable
-    public var json: JSON { .double(self) }
-}
-
-extension String: JSONConvertible {
-
-    @inlinable
-    public var json: JSON { .string(self) }
-}
-
-extension Array: JSONConvertible where Element == JSON {
-
-    @inlinable
-    public var json: JSON { .array(self) }
-}
-
-extension Dictionary: JSONConvertible where Key == String, Value == JSON {
-
-    @inlinable
-    public var json: JSON { .dictionary(self) }
 }
